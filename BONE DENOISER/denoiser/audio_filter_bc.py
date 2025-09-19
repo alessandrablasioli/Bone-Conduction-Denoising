4ab983aa8d4d1edbe2d6a03d39d8e9f36a7f5813
Alessandra Blasioli <80534938+alessandrablasioli@users.noreply.github.com>
2025-09-19 23:29:01 +0200

    Update audio_filter_bc.py

--- BONE DENOISER/denoiser/audio_filter_bc.py
+++ BONE DENOISER/denoiser/audio_filter_bc.py
@@ -1,9 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# author: adefossez
+# Authored by Alessandra Blasioli
 
 from collections import namedtuple
 import json
