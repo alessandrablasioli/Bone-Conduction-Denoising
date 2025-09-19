c6fc9ec1858ff9941d07211aa9c7174b7f789902
Alessandra Blasioli <80534938+alessandrablasioli@users.noreply.github.com>
2025-09-19 23:31:21 +0200

    Update enhance.py

--- BONE DENOISER/denoiser/enhance.py
+++ BONE DENOISER/denoiser/enhance.py
@@ -1,11 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates.t
-
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-# author: adiyoss
-
+# Authored by Alessandra Blasioli
 import argparse
 from concurrent.futures import ProcessPoolExecutor
 import json
