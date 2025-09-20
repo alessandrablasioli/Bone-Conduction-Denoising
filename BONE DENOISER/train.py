514ae6bd20412f8b43c9ded56f514e3ac7a1fbae
Alessandra Blasioli <80534938+alessandrablasioli@users.noreply.github.com>
2025-09-20 12:28:41 +0200

    last

--- BONE DENOISER/train.py
+++ BONE DENOISER/train.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 # authors: adiyoss and adefossez
-
+# This Version was authored by Alessandra Blasioli
 import logging
 import os
 
